37f8250a6dfdb08779d7250b2c889b480168b748
JungHyeonKang <rkdwjdgus0103@gmail.com>
2026-01-08 20:07:27 +0900

    Update App.js

--- my-app/src/App.js
+++ my-app/src/App.js
@@ -15,7 +15,7 @@ function App() {
           target="_blank"
           rel="noopener noreferrer"
         >
-          Learn GithubAction cicd222
+          Learn GithubAction cicd2-aws1
         </a>
       </header>
     </div>
